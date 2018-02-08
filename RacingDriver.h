a822404227d00bda4a975888df4c5c5b289f53a3
Alexandros Polychronopoulos <alexandros.polychronopoulos@cognityconsulting.gr>
2018-02-08 17:13:27 +0200

    first commit of Racing Drivers and Teams

---
+++ RacingDriver.h
@@ -0,0 +1,7 @@
+/**/
+#ifndef	__RACING_DRIVER_H__
+#define __RACING_DRIVER_H__
+
+include "RacingTeam.h"
+
+#endif
---
+++ RacingTeam.h
@@ -0,0 +1,9 @@
+/**/
+#ifndef	__RACING_TEAM_H__
+#define __RACING_TEAM_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif
